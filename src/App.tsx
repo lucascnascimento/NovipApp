ff16d481aca445f14703c59b64a2013b2a7b88f8
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-11-05 22:26:59 -0300

    Initial clean up

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+declare const global: {HermesInternal: null | {}};
+
+const App = () => {
+  return <Text>Hello World</Text>;
+};
+
+export default App;
